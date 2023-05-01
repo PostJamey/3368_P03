220a53a12f7acebc2ef280c3505001c60d45fe57
Maia P <lorien.legaycies@gmail.com>
2023-05-01 18:47:39 -0500

    BasicPlayer
    Basic player script added that lists out attributes in editor window. Materials added to player and stalker game objects.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("General Stats")]
+    [SerializeField]
+    private int _health;
+    [SerializeField]
+    private int _stamina;
+    [SerializeField]
+    private int _walkSpeed;
+    [SerializeField]
+    private int _runSpeed;
+    [SerializeField][Tooltip("The set amount of panic the player can endure before experiencing" +
+                            "into a panic attack")]
+    private int _panicLimit;
+    [SerializeField][Tooltip("The set amount of time the player experiences a panic attack ")]
+    private int _panicAttackDuration;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
