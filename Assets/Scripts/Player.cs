fdb1f0772a2d1c45c426160dd520aaeb064ec81d
Maia P <lorien.legaycies@gmail.com>
2023-05-01 23:31:29 -0500

    UI Added
    UI for health and stamina of player has been added. Player and Stalker scripts have been fleshed out significantly.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,17 +6,83 @@ public class Player : MonoBehaviour
 {
     [Header("General Stats")]
     [SerializeField]
-    private int _health;
+    private int _currentHealth;
     [SerializeField]
-    private int _stamina;
+    private int _maxHealth;
     [SerializeField]
-    private int _walkSpeed;
+    private int _currentStamina;
     [SerializeField]
-    private int _runSpeed;
-    [SerializeField][Tooltip("The set amount of panic the player can endure before experiencing" +
-                            "into a panic attack")]
+    private int _maxStamina;
+    [SerializeField]
+    private int _slowSpeed;
+    [SerializeField]
+    private int _normalSpeed;
+    [SerializeField]
+    private int _fastSpeed;
+    [SerializeField]
+    [Tooltip("The set amount of panic the player can endure before experiencing" +
+                        "into a panic attack")]
     private int _panicLimit;
-    [SerializeField][Tooltip("The set amount of time the player experiences a panic attack ")]
+    [SerializeField]
+    [Tooltip("The set amount of time the player experiences a panic attack ")]
     private int _panicAttackDuration;
 
+
+    //allowing values to be changed in inspector
+    public int CurrentHealth => _currentHealth;
+    public int MaxHealth => _maxHealth;
+    public int CurrentStamina => _currentStamina;
+    public int MaxStamina => _maxStamina;
+    public int SlowSpeed => _slowSpeed;
+    public int NormalSpeed => _normalSpeed;
+    public int FastSpeed => _fastSpeed;
+    public int PanicLimit => _panicLimit;
+    public int PanicAttackDuration => _panicAttackDuration;
+
+    public void TakeDamage(int amount)
+    {
+        _currentHealth -= amount;
+
+        if (_currentHealth <= 0)
+        {
+            //player is dead
+            //developer can choose death animation, game over screen, etc.
+
+            //play game over screen
+        }
+    }
+
+    public void Heal(int amount)
+    {
+        _currentHealth += amount;
+        //I want health to restore over time, like stamina
+
+        if (_currentHealth > _maxHealth)
+        {
+            _currentHealth = _maxHealth;
+        }
+    }
+
+    public void StaminaOut(int amount)
+    {
+        _currentStamina -= amount;
+
+        if (_currentStamina <= 0)
+        {
+            //player speed has now changed to slowSpeed
+            //call upon "slowSpeed" function here
+        }
+    }
+
+    public void StaminaRestore(int amount)
+    {
+        _currentStamina += amount;
+        //i want stamina to recover over time; not sure how to do that
+            //but may just need to add restoring items
+
+        if (_currentStamina > _maxStamina)
+        {
+            _currentStamina = _maxStamina;
+        }
+    }
 }
